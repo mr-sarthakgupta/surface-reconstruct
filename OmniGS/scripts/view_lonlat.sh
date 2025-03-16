e0ab192d6bea55ba767ec07d90e787d8aac5298a
Sarthak Gupta <81774392+mr-sarthakgupta@users.noreply.github.com>
2025-03-16 19:37:23 +0530

    Delete OmniGS directory

--- OmniGS/scripts/view_lonlat.sh
+++
@@ -1,4 +0,0 @@
-../bin/view_result \
-    ../cfg/view_only/view_only.yaml \
-    ../cfg/view_only/camera_lonlat.yaml \
-    /home/rapidlab/programs/omnigs/results/lonlat/EgoNeRF/Ricoh360_10/poster/32000/ply/point_cloud/iteration_32000/point_cloud.ply
